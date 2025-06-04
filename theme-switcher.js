676d8f4680f7a675e6fddca589ec323c95c93da1
Sergejs Rakovskis <sr19062@edu.lu.lv>
2025-06-04 21:58:25 +0300

    Major patch

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,9 +1,11 @@
 const toggleBtn = document.getElementById('theme-toggle');
+const toggleBtn2 = document.getElementById('theme-toggle2');
 const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 function setTheme(dark) {
     document.body.classList.toggle('dark-theme', dark);
     toggleBtn.textContent = dark ? '🌞' : '🌚';
+    toggleBtn2.textContent = dark ? '🌞' : '🌚';
     localStorage.setItem('theme', dark ? 'dark' : 'light');
 }
 
@@ -16,3 +18,8 @@ toggleBtn.addEventListener('click', () => {
     const isDark = document.body.classList.contains('dark-theme');
     setTheme(!isDark);
 });
+
+toggleBtn2.addEventListener('click', () => {
+    const isDark = document.body.classList.contains('dark-theme');
+    setTheme(!isDark);
+})
